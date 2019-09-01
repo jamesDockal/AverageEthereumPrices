8c0bc174d2fdc524971320e4e6b56d882c924f5a
jamelDock <48695399+jamelDock@users.noreply.github.com>
2019-09-01 12:08:52 -0300

    Update AverageEthereumPrices.js

--- AverageEthereumPrices.js
+++ AverageEthereumPrices.js
@@ -1,36 +1,47 @@
 const fs = require('fs')
 
-const priceList = fs.readFileSync('C:\\Users\\james\\Desktop\\javascript\\infoList.txt', 'utf-8').split('\n')
+// put your directory here, where the files are
+const Yourdirectory = 'C:\\Users\\james\\Desktop\\javascript'
 
-let daysToAverage = 7
-let lineCounter = 1
-let sums = {
-  open: 0,
-  close: 0
-}
-let averagesString = ''
-for (price of priceList) {
-  let [data, open, high, low, close, volume, marketCap] = price.replace(/,/g, '.').replace('.', ',').split('\t')
+try{
+  const priceList = fs.readFileSync(`${Yourdirectory}\\AverageEthereumPrices\\infoList.txt`, 'utf-8').split('\n') 
 
-  sums.open += parseFloat(open)
-  sums.close += parseFloat(close)
-
-  if (lineCounter % daysToAverage === 0) {
-    const averages = calculateAverage(sums)
-    averagesString += `${averages.open}\t${averages.close}\n` 
+  let daysToAverage = 7
+  let lineCounter = 1
+  let sums = {
+    open: 0,
+    close: 0
   }
-  lineCounter++
-}
-fs.writeFileSync('C:\\Users\\james\\Desktop\\javascript\\Averages.txt', averagesString, {encoding: 'utf-8', flag: 'w+'})
-
-function calculateAverage(sum){
-  let averages = {
-    open: sum.open/daysToAverage,
-    close: sum.close/daysToAverage
+  let averagesString = ''
+  for (price of priceList) {
+    let [data, open, high, low, close, volume, marketCap] = price.replace(/,/g, '.').replace('.', ',').split('\t')
+  
+    sums.open += parseFloat(open)
+    sums.close += parseFloat(close)
+  
+    if (lineCounter % daysToAverage === 0) {
+      const averages = calculateAverage(sums)
+      averagesString += `${averages.open}\t${averages.close}\n` 
+    }
+    lineCounter++
   }
-
-  sum.open = 0
-  sum.close = 0
-
-  return averages
+  try{
+    fs.writeFileSync(`${Yourdirectory}\\AverageEthereumPrices\\Averages.txt`, averagesString, {encoding: 'utf-8', flag: 'w+'})
+  }catch(err){
+    console.log(err)
+  }
+  
+  function calculateAverage(sum){
+    let averages = {
+      open: sum.open/daysToAverage,
+      close: sum.close/daysToAverage
+    }
+  
+    sum.open = 0
+    sum.close = 0
+  
+    return averages
+  }
+}catch(err){
+  console.log(err)
 }
